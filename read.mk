b7828b6e5cbd71ce2efe0d672509b94f884a932b
Lionszhang <zqhusixia@163.com>
2017-07-13 20:40:53 +0800

    增加通信模块

--- read.mk
+++ read.mk
@@ -19,3 +19,24 @@
    git remote add origin https://github.com/LionsZhang/ReactTestProject.git # 添加远程仓库地址
 
    git remote set-url origin https://github.com/LionsZhang/ReactTestProject.git # 设置远程仓库地址(用于修改远程仓库地址) git remote rm <repository> # 删除远程仓库
+有如下几种解决方法：
+
+1.使用强制push的方法：
+
+$ git push -u origin master -f
+
+这样会使远程修改丢失，一般是不可取的，尤其是多人协作开发的时候。
+
+2.push前先将远程repository修改pull下来
+
+$ git pull origin master
+
+$ git push -u origin master
+
+3.若不想merge远程和本地修改，可以先创建新的分支：
+
+$ git branch [name]
+
+然后push
+
+$ git push -u origin [name]
